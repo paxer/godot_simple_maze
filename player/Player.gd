92e7d850d979bb5e41b289cbdb63814e4d90954d
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2020-07-28 22:02:10 +1000

    implement simple player movment and inital maze level

---
+++ player/Player.gd
@@ -0,0 +1,18 @@
+extends KinematicBody
+class_name Player
+
+export var move_speed : float = 10.0
+export var rotate_speed : float = 3.0
+
+var acceleration = 1.1
+var friction = 1.44
+
+func _physics_process(delta: float) -> void:
+	var z_movement : float = Input.get_action_strength("backward") - Input.get_action_strength("forward")
+	var rotate : float = Input.get_action_strength("left") - Input.get_action_strength("right")
+	
+	rotation.y += rotate * rotate_speed * delta
+	var direction : Vector3 = Vector3(0, 0, 1).rotated(Vector3(0, 1, 0), rotation.y)
+	var motion : Vector3 = direction * z_movement * delta * move_speed	
+
+	move_and_collide(motion)
